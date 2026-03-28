c5f64346264593945ca85532385c33bb6e3dbbaa
juancacrc <juan.rodriguezc@est.iudigital.edu.co>
2026-03-28 17:27:35 -0500

    Add PersonalAdministrativo class definition
    se adicionan archivos con superclase, subclases, herencias, y se detallan polimorfismo y demás requerimientos del proyecto

---
+++ src/main/java/com/universidad/personas/PersonalAdministrativo.java
@@ -0,0 +1,7 @@
+
+package com.universidad.personas;
+
+public class PersonalAdministrativo extends Persona {
+    private String numeroEmpleado;
+    private String cargo;
+    private String departamento;
